36087933c7f36176ce5369db1935fbffbd89283f
Nadeen Noaman <95418928+nnoaman@users.noreply.github.com>
2025-01-25 22:17:31 +0100

    Add the deployer as well

--- .github/workflows/scripts/update-workflow-on-workspaces-changes.sh
+++ .github/workflows/scripts/update-workflow-on-workspaces-changes.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
 # Remove the current values
+yq -i 'del(.on.workflow_dispatch.inputs.deployer.options)' .github/workflows/01-deploy-control-plane.yaml
 yq -i 'del(.on.workflow_dispatch.inputs.library.options)' .github/workflows/01-deploy-control-plane.yaml
 
 # Add the new values
+for deployer in $(ls ${GITHUB_WORKSPACE}/WORKSPACES/DEPLOYER); do
+  yq -i '.on.workflow_dispatch.inputs.deployer.options += ["'${deployer}'"]' .github/workflows/01-deploy-control-plane.yaml
+done
+
 for library in $(ls ${GITHUB_WORKSPACE}/WORKSPACES/LIBRARY); do
   yq -i '.on.workflow_dispatch.inputs.library.options += ["'${library}'"]' .github/workflows/01-deploy-control-plane.yaml
 done
 
+# Check for changes and commit if there are any
 if git diff --exit-code; then
   echo "No changes to the deployment workflow."
   exit 0
